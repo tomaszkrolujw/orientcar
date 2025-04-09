5a4149362472fc5158481acb732baa33e83ed3de
Carol-199 <4040@student.ujw.pl>
2025-04-09 21:33:22 +0200

    initial commit projektu OrientCar

---
+++ src/main/java/pl/orientcar/OrientCarApplication.java
@@ -0,0 +1,11 @@
+package pl.orientcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientCarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrientCarApplication.class, args);
+    }
+}
---
+++ src/main/java/pl/orientcar/SpringBootApplication.java
@@ -0,0 +1,4 @@
+package pl.orientcar;
+
+public @interface SpringBootApplication {
+}
